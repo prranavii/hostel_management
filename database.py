65de394c7e79ee616ed4825f87d38ee8daaea8b7
prranavii <pranavijain47@gmail.com>
2024-05-20 13:05:53 +0530

    initial commit

---
+++ database.py
@@ -0,0 +1,68 @@
+import mysql.connector
+
+conn=mysql.connector.connect(host="localhost",user="root",password="root",database="hostel_management")
+
+cursor=conn.cursor()
+
+
+def add_student(name,admission_no, course,phone, room_no=0):
+    try:
+        insert_query= "INSERT INTO students (name,room_no,admission_no,course,phone) VALUES(%s,%s,%s,%s,%s)"
+        student_data=(name,room_no,admission_no,course,phone)
+        cursor.execute(insert_query,student_data)
+
+        conn.commit()
+
+        return True
+    except mysql.connector.Error as e:
+        print("Error ", e)  
+        return False
+
+def add_room(room_no,no_of_students,ac):
+    try:
+        insert_query= "INSERT INTO room (room_no,no_of_students,ac) VALUES (%s,%s,%s)"
+        room_data=(room_no,no_of_students,ac)
+        cursor.execute(insert_query,room_data)
+
+        conn.commit()
+
+        return True
+    except mysql.connector.Error as e:
+        print("error",e)
+        return False
+    
+def view_students(name=None,phone=None,):
+    try:
+        select_query=""
+        if name is not None:
+            set_query = f"SELECT * FROM students WHERE name={name}"
+        elif phone is not None:
+            set_query = f"SELECT * FROM phones WHERE phone={phone}"
+        else:
+            select_query = "SELECT * FROM students"
+        
+        cursor.execute(select_query)
+
+        students=cursor.fetchall()
+        return students
+    
+    except mysql.connector.Error as e:
+        print("error",e)
+        return False
+
+def allot_room(phone_no,room_no):
+    try:
+        update_query=f"update students set room_no={room_no} where phone= {phone_no}"
+        
+        cursor.execute(update_query)
+        conn.commit()
+
+        if cursor.rowcount() == 0:
+            return 0
+        else: 
+            return True
+    
+    except mysql.connector.Error as e:
+        print("error",e)
+        return False
+
